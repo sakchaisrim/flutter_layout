01d0d2e2a330ea66c10a02cca32b2e230a3974b6
sakchaisrim <sakchaisrim@gmail.com>
2023-11-27 15:46:05 +0700

    update

--- lib/MyScreen2.dart
+++ lib/MyScreen2.dart
@@ -14,10 +14,41 @@ class MyScreen2 extends StatelessWidget {
         child: Container(
           height: 300,
           width: 300,
-          color: Colors.orange[200],
-          padding: EdgeInsets.symmetric(
-            vertical: 50,
-            horizontal: 25,
+          decoration: BoxDecoration(
+            color: Colors.orange[200],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          padding: EdgeInsets.all(25),
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  'Sakchai Srimakorn',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.green[600],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(Icons.home,
+                    color: Colors.pink[300],
+                    size: 50,
+                    ),
+                    Icon(Icons.star,
+                    color: Colors.yellow[300],
+                    size: 50,
+                    ),
+                    Icon(Icons.favorite,
+                    color: Colors.blue[300],
+                    size: 50,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
